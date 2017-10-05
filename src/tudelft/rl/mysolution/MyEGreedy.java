56922ff1aa15c97b9881af87946d1bfc9e783295
Martijn Vermeulen <martijn01.vermeulen@gmail.com>
2017-10-05 16:46:05 +0200

    First two functions implemented
    Retrieving best and random action

--- src/tudelft/rl/mysolution/MyEGreedy.java
+++ src/tudelft/rl/mysolution/MyEGreedy.java
@@ -1,23 +1,44 @@
 package tudelft.rl.mysolution;
 
-import tudelft.rl.Action;
-import tudelft.rl.Agent;
-import tudelft.rl.EGreedy;
-import tudelft.rl.Maze;
-import tudelft.rl.QLearning;
+import tudelft.rl.*;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class MyEGreedy extends EGreedy {
 
+	/**
+	 *
+	 * @param r
+	 * @param m
+	 * @return
+	 */
 	@Override
 	public Action getRandomAction(Agent r, Maze m) {
-		//TODO to select an action at random in State s
-		return null;
+		ArrayList<Action> possibleActions = m.getValidActions(r);
+		Random randomGenerator = new Random();
+		int randomIndex = randomGenerator.nextInt(possibleActions.size());
+
+		return possibleActions.get(randomIndex);
 	}
 
 	@Override
 	public Action getBestAction(Agent r, Maze m, QLearning q) {
 		//TODO to select the best possible action currently known in State s.
-		return null;
+		ArrayList<Action> possibleActions = m.getValidActions(r);
+		State currentState = r.getState(m);
+
+		Action best = getRandomAction(r, m);
+
+		for(int i = 1; i<possibleActions.size();i++){
+			Action possibleAction = possibleActions.get(i);
+
+			if(q.getQ(currentState,possibleAction)>q.getQ(currentState,best)){
+				best = possibleAction;
+			}
+		}
+
+		return best;
 	}
 
 	@Override
