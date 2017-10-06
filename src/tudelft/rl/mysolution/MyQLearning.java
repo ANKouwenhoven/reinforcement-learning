9dc0d76ab80cfaf2a8b846e5d69a14bc24fb62cf
Martijn Vermeulen <martijn01.vermeulen@gmail.com>
2017-10-06 13:55:48 +0200

    Working algorithm
    Updated updateQ function, now working.
    Added result with only choosing best actions to check if the solution is the shortest.

--- src/tudelft/rl/mysolution/MyQLearning.java
+++ src/tudelft/rl/mysolution/MyQLearning.java
@@ -8,29 +8,28 @@ import tudelft.rl.State;
 
 public class MyQLearning extends QLearning {
 
+    /**
+     * Updating the Q value for the state the agent left.
+     * @param s, state where the agent came from
+     * @param a, action it executed
+     * @param r, the reward in the resulting state
+     * @param s_next, the resulting state
+     * @param possibleActions, the available actions in the resulting state
+     * @param alfa, learning rate alpha
+     * @param gamma, discount factor
+     */
 	@Override
 	public void updateQ(State s, Action a, double r, State s_next, ArrayList<Action> possibleActions, double alfa, double gamma) {
-
-		Action bestAction = possibleActions.get(0);
+        Action bestAction = possibleActions.get(0);
 
 		for(Action possibleAction : possibleActions) {
-			if(getQ(s, possibleAction) > getQ(s, bestAction)) {
+			if(getQ(s_next, possibleAction) > getQ(s_next, bestAction)) {
 				bestAction = possibleAction;
 			}
 		}
 
 		double addedValue = alfa*(r + gamma * getQ(s_next,bestAction) - getQ(s,a));
-
-
 		double newQ = getQ(s,a) + addedValue;
-
-		/*
-		if(addedValue!=0.0){
-			System.out.println("Added value: " + addedValue);
-			System.out.println("Update Q from: " + getQ(s,a) + " to: " + newQ);
-
-		}*/
-
 
 		setQ(s, a, newQ);
 		
--- src/tudelft/rl/mysolution/RunMe.java
+++ src/tudelft/rl/mysolution/RunMe.java
@@ -11,11 +11,11 @@ public class RunMe {
 		
 		//load the maze
 		//TODO replace this with the location to your maze on your file system
-		Maze maze = new Maze(new File("C:\\toy_maze.txt"));
+		Maze maze = new Maze(new File("C:\\easy_maze.txt"));
 
 
-		int goalX = 9;
-		int goalY = 9;
+		int goalX = 24;
+		int goalY = 14;
 		//Set the reward at the bottom right to 10
 		maze.setR(maze.getState(goalX, goalY), 10);
 
@@ -34,8 +34,8 @@ public class RunMe {
 		int steps = 0;
 		int trials = 0;
 		boolean checkSteps = false;
-		int criterionActions = 10000;
-		int criterionTrials = 100;
+		int criterionActions = 1000;
+		int criterionTrials = 10000;
 
 		double alpha = 0.7;
 		double gamma = 0.9;
@@ -45,26 +45,25 @@ public class RunMe {
 
 		//keep learning until you decide to stop
 		while (!stop) {
+
+
 			//TODO implement the action selection and learning cycle
-			Action nextAction = selection.getEGreedyAction(robot, maze, learn, epsilon);
-			State s = robot.getState(maze);
 
+			// New action, get the state and reward in that state
+			State previous_state = robot.getState(maze);
+			Action executedAction = selection.getEGreedyAction(robot, maze, learn, epsilon);
+			State new_state = robot.doAction(executedAction,maze);
+			double reward = maze.getR(new_state);
 
-			State new_state = robot.doAction(nextAction,maze);
-			double reward = maze.getR(s);
 			ArrayList<Action> possibleActions = maze.getValidActions(robot);
 
-			if(reward>0){
-				System.out.println("Found a reward");
-			}
-			learn.updateQ(s, nextAction, reward, new_state, possibleActions, alpha, gamma);
+			learn.updateQ(previous_state, executedAction, reward, new_state, possibleActions, alpha, gamma);
 
 
 			if(robot.x == goalX && robot.y == goalY){
 				robot.reset();
 				trials++;
 			}
-
 
 			steps++;
 
@@ -76,6 +75,17 @@ public class RunMe {
 				stop = true;
 			}
 		}
+
+		robot.reset();
+
+		System.out.println("Now only do the best actions, number of actions:");
+
+		// Run it one more time
+		while(!(robot.x == goalX && robot.y == goalY)){
+			Action executedAction = selection.getBestAction(robot, maze, learn);
+			robot.doAction(executedAction,maze);
+		}
+		robot.reset();
 
 	}
 
