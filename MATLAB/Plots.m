1986cfdb02f4ae2d9f5cbd3c880ffb387a181861
Martijn Vermeulen <martijn01.vermeulen@gmail.com>
2017-10-11 11:16:58 +0200

    Data and plots for 2.1.6
    Added plots and data for development

--- MATLAB/Plots.m
+++ MATLAB/Plots.m
@@ -22,3 +22,22 @@ plot(1:20,averageEasy);
 title('Performance average of 10 runs in Easy Maze');
 ylabel('Actions per trial');
 xlabel('Trial number');
+
+%%
+
+load('Data6_toy.mat');
+load('Data6_easy.mat');
+
+figure;
+averageToy = mean(Data6_toy(:,:),2);
+plot(1:50,averageToy);
+title('Performance average of 10 runs in Toy Maze with Q-learning');
+ylabel('Actions per trial');
+xlabel('Trial number');
+
+figure;
+averageEasy = mean(Data6_easy(:,:),2);
+plot(1:100,averageEasy);
+title('Performance average of 10 runs in Easy Maze with Q-learning');
+ylabel('Actions per trial');
+xlabel('Trial number');
